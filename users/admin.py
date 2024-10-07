252f5a7ebd14250b9647539a2edde14aec23a0ec
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-10-07 18:43:28 +0500

    created user model, serializer,views and urls

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone", "is_staff", "is_active")
+    search_fields = ("id", "email", "phone")
+    list_filter = ("is_active",)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"blank": True, "null": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True, verbose_name="email", help_text="Email address"
+    )
+    phone = models.CharField(
+        max_length=35, verbose_name="phone", help_text="Enter phone number", **NULLABLE
+    )
+
+    city = models.CharField(
+        max_length=100, verbose_name="city", help_text="Enter city", **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"Пользователь №{self.pk}. Email: {self.email}"
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
