f30e985a9c08f5434f8885fb3fa56b724f67e8c6
Ivan Ukhov <ivan.ukhov.work@gmail.com>
2024-10-07 19:33:13 +0500

    network models created

--- users/urls.py
+++ users/urls.py
@@ -5,14 +5,15 @@ from users.apps import UsersConfig
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
 from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = ([
 
-    path('register/', UserCreateAPIView.as_view(permission_classes=(AllowAny,)), name='register'),
-    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
+    path('register/', UserCreateAPIView.as_view(permission_classes=(AllowAny, )), name='register'),
+    path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny, )), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny, )), name='token_refresh'),
 
 ])
